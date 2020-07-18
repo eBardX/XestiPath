30eab117f1c7dc61bfb86574faf55cbb3eded904
J. G. Pusey <ebardx@gmail.com>
2020-07-18 12:34:16 -0400

    Updated to support Swift 5.2

--- Package.swift
+++ Package.swift
@@ -1,10 +1,12 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
-// © 2018 J. G. Pusey (see LICENSE.md)
+// © 2018–2020 J. G. Pusey (see LICENSE.md)
 
 import PackageDescription
 
 let package = Package(name: "XestiPath",
+                      platforms: [.macOS(.v10_15)],
                       products: [.library(name: "XestiPath",
                                           targets: ["XestiPath"])],
-                      targets: [.target(name: "XestiPath")])
+                      targets: [.target(name: "XestiPath")],
+                      swiftLanguageVersions: [.version("5")])
